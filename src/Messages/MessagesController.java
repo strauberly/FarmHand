4c51ac238b9f1fb0a2e2fe4ad6d7066939397aa7
Brian Crookshanks <zeron30@hotmail.com>
2021-12-15 07:53:46 -0500

    project overhaul /out/artifacts contains linux executable jar

---
+++ src/Messages/MessagesController.java
@@ -0,0 +1,103 @@
+package Messages;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.effect.Glow;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MessagesController implements Initializable {
+        public Rectangle messagesBanner;
+        @FXML
+        private Rectangle messagesSquare;
+        @FXML
+        private Button messagingMinButton;
+        @FXML
+        private Label messagesNotes;
+
+        private static boolean maxed = false;
+        private static final Rectangle2D displaySize = Screen.getPrimary().getBounds();
+
+
+        public void messagingMinEnter(MouseEvent mouseEvent) {
+            messagingMinButton.setEffect(new Glow(.25));
+        }
+
+        public void messagingMinExit(MouseEvent mouseEvent) {
+            messagingMinButton.setEffect(new Glow(.0));
+        }
+
+        public void messagingMinPressed(MouseEvent mouseEvent) {
+            messagingMinButton.setEffect(new Glow(.80));
+
+        }
+
+        public void messagingMinReleased(MouseEvent mouseEvent) {
+            messagingMinButton.setEffect(new Glow(.0));
+            Stage stage = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+            stage.setIconified(true);
+        }
+
+
+        public Button messagingCloseButton;
+
+        public void messagingCloseEnter(MouseEvent mouseEvent) {
+            messagingCloseButton.setEffect(new Glow(.25));
+        }
+
+        public void messagingCloseExit(MouseEvent mouseEvent) {
+            messagingCloseButton.setEffect(new Glow(.0));
+        }
+
+        public void messagingClosePressed(MouseEvent mouseEvent) {
+            messagingCloseButton.setEffect(new Glow(.80));
+        }
+
+        public void messagingCloseReleased(MouseEvent mouseEvent) {
+            messagingCloseButton.setEffect(new Glow(.0));
+            Stage stage = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+            stage.close();
+        }
+
+        @Override
+        public void initialize(URL url, ResourceBundle resourceBundle) {
+            messagesNotes.setText("""
+                                                                            Behold!
+                                                   This Section Is Under Development.
+                                     
+                Imagine this... a place... for mAH d00ds... to stop, collaborate, and listen!
+                                
+                Possibly forum style, possibly messenger style? 
+                               
+                How does that sound to mAH d00d?
+                               
+                Far out... Will get back to you on that.
+
+                 """);
+
+            if (maxed) {
+                messagesBanner.setWidth(displaySize.getWidth());
+                maxed = true;
+
+                messagesNotes.setFont(Font.font(28));
+                messagesNotes.setPrefHeight(900);
+                messagesNotes.setPrefWidth(1500);
+                messagesNotes.setTranslateX(170);
+
+                messagesSquare.setHeight(900);
+                messagesSquare.setWidth(1500);
+                messagesSquare.setTranslateX(240);
+
+            }
+        }
+    }
---
+++ src/Weather/weatherdb/WDBHourly.java
@@ -0,0 +1,147 @@
+package Weather.weatherdb;
+import java.sql.*;
+
+//contains constructor for writing to viewable table
+public class WDBHourly {
+    // init variables
+    private final String time;
+    private String pressure;
+    private String temperature;
+    private String wind;
+    private String humidity;
+
+    // calculation variables
+    private static String timeFieldValue;
+    private static String pressureFieldValue;
+    private static String temperatureFieldValue;
+    private static String windFieldValue;
+    private static String humidityFieldValue;
+    private static long timeStamp;
+
+    //constructor
+    WDBHourly(String pressure, String temperature, String wind, String humidity, String time) {
+        this.pressure = pressure;
+        this.temperature = temperature;
+        this.wind = wind;
+        this.humidity = humidity;
+        this.time = time;
+    }
+
+    // write to hourly table in weather database
+    public static void writeToHourly() throws SQLException {
+        Connection conn = DriverManager.getConnection(WeatherDB.CONNECTION_STRING);
+        Statement statement = conn.createStatement();
+        statement.executeUpdate("INSERT INTO hourly (timestamp, pressure, temperature, wind, humidity)" +
+                "VALUES ( " + timeStamp + ", " + pressureFieldValue + ", "
+                + temperatureFieldValue + ", " + windFieldValue + ", " + humidityFieldValue + ")");
+        observableTimeConversion();
+        statement.executeUpdate("UPDATE hourly SET time = ('" + timeFieldValue + "')" + "WHERE hourly_id_ = (SELECT max (hourly_id_) FROM hourly)");
+        statement.close();
+        conn.close();
+    }
+// ** feed back method
+//     protected static void printHourlyLog() {
+//        System.out.println(
+//                "Wrote to hourly: " +
+//                        "\n" + WDBHourly.getCurrentTime() +
+//                        "\n" + "Pressure = " + pressureFieldValue +
+//                        "\n" + "Temperature = " + temperatureFieldValue +
+//                        "\n" + "Wind = " + windFieldValue +
+//                        "\n" + "Humidity = " + humidityFieldValue +
+//                        "\n" + "Hourly entries = "
+//                        + WeatherDB.getID("hourly_id_","hourly", getHourlyEntries) + "\n"
+//        );
+//    }
+//    *****************************************************
+
+    //methods convert time returned in milliseconds into a human-readable format then written to hourly table
+    public static void observableTimeConversion() throws SQLException {
+        try {
+            Connection conn = DriverManager.getConnection(WeatherDB.CONNECTION_STRING);
+            Statement statement = conn.createStatement();
+            String sql = "SELECT timestamp FROM hourly";
+            ResultSet rs = statement.executeQuery(sql);
+            while (rs.next()) {
+                long loggedTime = rs.getLong("timestamp");
+                timeFieldValue = convertedLoggedTime(loggedTime);
+            }
+            rs.close();
+            statement.close();
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private static String convertedLoggedTime(Long time) {
+        return String.valueOf(new Time(time));
+    }
+
+    // getters and setters
+
+    public static void setTimeStamp(long timeStamp) {
+        WDBHourly.timeStamp = timeStamp;
+    }
+
+    public static long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public static void setPressureFieldValue(String pressureFieldValue) {
+        WDBHourly.pressureFieldValue = pressureFieldValue;
+    }
+
+    public static void setTemperatureFieldValue(String temperatureFieldValue) {
+        WDBHourly.temperatureFieldValue = temperatureFieldValue;
+    }
+
+    public static void setWindFieldValue(String windFieldValue) {
+        WDBHourly.windFieldValue = windFieldValue;
+    }
+
+    public static void setHumidityFieldValue(String humidityFieldValue) {
+        WDBHourly.humidityFieldValue = humidityFieldValue;
+    }
+
+    public static String getTimeFieldValue() {
+        return timeFieldValue;
+    }
+
+    public void setPressure(String pressure) {
+        this.pressure = pressure;
+    }
+
+    public void setTemperature(String temperature) {
+        this.temperature = temperature;
+    }
+
+    public void setWind(String wind) {
+        this.wind = wind;
+    }
+
+    public void setHumidity(String humidity) {
+        this.humidity = humidity;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getPressure() {
+        return pressure;
+    }
+
+    public String getTemperature() {
+        return temperature;
+    }
+
+    public String getWind() {
+        return wind;
+    }
+
+    public String getHumidity() {
+        return humidity;
+    }
+
+}
