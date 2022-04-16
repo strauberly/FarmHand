fc1082917abe56dc1a730f357bef2beb625a4a8e
Brian Crookshanks <zeron30@hotmail.com>
2022-04-16 13:52:03 -0700

    new chore window roughed out

--- src/Chores/DataModel/OutsideChore.java
+++
@@ -1,9 +0,0 @@
-package Chores.DataModel;
-
-
-public class OutsideChore extends Chore{
-
-    public OutsideChore(String choreName, String description, String priority, String dateCreated, String dateDue) {
-        super(choreName, description, priority, dateCreated, dateDue);
-    }
-}
